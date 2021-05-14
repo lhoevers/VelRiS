fc82baf08d5620143058cdfdc9d36db896ab791b
lhoevers <71526875+lhoevers@users.noreply.github.com>
2021-05-14 16:58:20 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,59 @@
+import json
+import os
+import datetime
+import csv
+
+# if no json settings file is found use empty dict
+# create function for this
+def config_create():
+    if os.path.exists("config.json") == False:
+        settings = dict({
+            "competition" : dict({
+                "checkpointteam" : "Na",
+                "checkpointteam_list" : "Na",
+                "checkpoint" : "Na",
+                "checkpoint_list" : "Na"
+            }),
+            "system" : dict({
+                "id" : "Na",
+                "battery" : "Na",
+                "internet": "Na",
+                "update_interval" : "Na",
+                "file_name" : "Na",
+            }),    
+            "log" : dict({
+                "update_last" : "Na",
+                "record_last" : "Na",
+            })
+        })
+
+        with open('config.json', 'w') as outfile:
+            json.dump(settings, outfile, indent = 4)
+
+
+# read json file for all settings
+def config_read():
+    with open('config.json') as json_file:
+        config = json.load(json_file)
+    return(config)
+
+# write json file for all settings
+def config_write(config):
+    log(config)
+
+    with open('config.json', 'w') as outfile:
+        json.dump(config, outfile, indent = 4)
+
+# logging of config
+def log(config_new):
+    config_old = config_read()
+
+    datetime_current = datetime.datetime.now().astimezone().replace(microsecond=0).isoformat() #current system datetime
+
+    for key_1 in config_old.keys():
+        for key_2 in config_old[key_1].keys():
+            if config_old[key_1].get(key_2) != config_new[key_1].get(key_2):
+                log_record = [datetime_current, key_1, key_2, config_old[key_1].get(key_2), config_new[key_1].get(key_2)]
+                with open('log.csv','a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(log_record)
---
+++ main.py
@@ -0,0 +1,69 @@
+import threading
+import csv
+import time, datetime
+
+import webapp.webapp #local webserver for entering manual data
+import RFID.RFID #RFID reader interaction
+import config #import the config of the app
+
+
+def save_data(data): #function to save data from memory to file
+    checkpoint = config.config_read()["competition"]["checkpoint"] #get current checkpoint
+    checkpointteam = config.config_read()["competition"]["checkpointteam"] #get current checkpointteam
+
+    data = [list(tup)+[checkpoint, checkpointteam] for tup in data] #add checkpoint and checkpointteam to dataset in buffer
+    
+    file_name = config.config_read()["system"]["file_name"] #get file_name from config
+    with open(file_name,'a') as f: #write buffer to file
+        writer = csv.writer(f)
+        writer.writerows(data)
+
+
+def cloud_synchronization():
+    pass
+
+
+def flaskThread(): #function to start local webserver
+    webapp.webapp.app.run(host="0.0.0.0", threaded=True)
+
+
+# start and keep reading RFID reader from the class RFID
+def RFIDThread():  
+    RFID.RFID.RFID().start()
+
+
+# read the buffer of the RFID class
+def bufferloop_thread():  
+    while True: #start endless loop
+        cloud_synchronization() #function for cloud synchronization
+
+        update_interval = config.config_read()["system"]["update_interval"] #get current checkpoint
+
+        # buffer RFID to file
+        if len(RFID.RFID.RFID.buffer) > 0: # If the buffer is not empty
+            buffer_save = RFID.RFID.RFID.buffer #save buffedata to local variable
+            
+            save_data(buffer_save) #write buffer data to function save data
+            RFID.RFID.RFID.buffer = list(set(RFID.RFID.RFID.buffer)-set(buffer_save)) #remove saved data from the buffer of the RFID class
+
+        # buffer webapp to file    
+        if len(webapp.webapp.buffer) > 0: # If the buffer is not empty
+            buffer_save = webapp.webapp.buffer #save buffedata to local variable
+            
+            save_data(buffer_save) #write buffer data to function save data
+            webapp.webapp.buffer = list(set(webapp.webapp.buffer)-set(buffer_save)) #remove saved data from the buffer of the webapp
+       
+        cloud_synchronization() # send all data to the cloud
+
+        time.sleep(update_interval) #wait update interval
+        
+
+
+if __name__ == '__main__':
+    config.config_create() #create config file if absent
+    threading.Thread(target=flaskThread).start() #start theard for webserver
+    threading.Thread(target=RFIDThread).start() #start thread for interaction with RFID reader
+    threading.Thread(target=bufferloop_thread).start() #start thread for redaing buffers
+    
+    
+    
